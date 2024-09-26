74ab2b58152329371d451434717fd6d0b8f0cca4
kylehoahoang <128729118+kylehoahoang@users.noreply.github.com>
2024-09-26 01:30:54 -0400

    ui changes + modularization

--- loungelizard/src/main.rs
+++ loungelizard/src/main.rs
@@ -34,9 +34,20 @@ pub fn App() -> Element {
 #[component]
 fn Home() -> Element {
     let mut show_login_pane = use_signal(|| false);
-    let mut show_server_pane = use_signal(|| false);
-    let mut token = use_signal(|| "".to_string());
-    let mut guilds = use_signal(|| Value::Null);
+    let mut show_discord_login_pane = use_signal(|| false);
+    let mut show_discord_server_pane = use_signal(|| false);
+    let mut discord_token = use_signal(|| "".to_string());
+    let mut discord_guilds = use_signal(|| Value::Null);
+
+    let handle_discord_click = move |_| {
+        if discord_token.to_string() == "" {
+            show_login_pane.set(!show_login_pane()); 
+            show_discord_login_pane.set(!show_discord_login_pane());
+        }
+        else {
+            show_discord_server_pane.set(!show_discord_server_pane());
+        }
+    };
 
     rsx! {
         div {
@@ -52,7 +63,7 @@ fn Home() -> Element {
                     width: "50px",
                     height: "50px",
                     style: "cursor: pointer;",
-                    onclick: move |_| show_login_pane.set(!show_login_pane()),
+                    onclick: handle_discord_click,
                 }
             }
 
@@ -68,19 +79,20 @@ fn Home() -> Element {
                     class: {
                         format_args!("login-pane {}", if show_login_pane() { "show" } else { "" })
                     },
-                    Login { 
+                    DiscordLogin { 
                         show_login_pane: show_login_pane.clone(), 
-                        show_server_pane: show_server_pane.clone(), 
-                        token: token.clone(),
-                        guilds: guilds.clone(),
+                        show_discord_login_pane: show_discord_login_pane.clone(),
+                        show_discord_server_pane: show_discord_server_pane.clone(), 
+                        discord_token: discord_token.clone(),
+                        discord_guilds: discord_guilds.clone(),
                     }, 
                 }
 
                 // Bottom pane for servers
-                BottomPane { 
-                    show_server_pane: show_server_pane.clone(),
-                    guilds: guilds.clone(),
-                    token: token.clone()
+                DiscordBottomPane { 
+                    show_discord_server_pane: show_discord_server_pane.clone(),
+                    discord_guilds: discord_guilds.clone(),
+                    discord_token: discord_token.clone()
                 }, 
             }
         }
@@ -89,7 +101,7 @@ fn Home() -> Element {
 
 
 #[component]
-fn Login(show_login_pane: Signal<bool>, show_server_pane: Signal<bool>, token: Signal<String>, guilds: Signal<Value>) -> Element {
+fn DiscordLogin(show_login_pane: Signal<bool>, show_discord_login_pane: Signal<bool>, show_discord_server_pane: Signal<bool>, discord_token: Signal<String>, discord_guilds: Signal<Value>) -> Element {
     let mut username = use_signal(|| "example@gmail.com".to_string());
     let mut password = use_signal(|| "password".to_string());
 
@@ -101,10 +113,11 @@ fn Login(show_login_pane: Signal<bool>, show_server_pane: Signal<bool>, token: S
 
         block_on(async move {
             match discord_api::login_request(username.to_string(), password.to_string()).await {
-                Ok((user_id, auth_token)) => {
-                    token.set(auth_token); // Call the success handler
+                Ok((user_id, auth_discord_token)) => {
+                    discord_token.set(auth_discord_token); // Call the success handler
                     show_login_pane.set(false);
-                    show_server_pane.set(true);
+                    show_discord_login_pane.set(false);
+                    show_discord_server_pane.set(true);
                     info!("Login successful");
                 }
                 Err(e) => {
@@ -115,14 +128,14 @@ fn Login(show_login_pane: Signal<bool>, show_server_pane: Signal<bool>, token: S
         });
 
         block_on(async move {
-            match discord_api::get_guilds(token.to_string()).await {
-                Ok((guilds_response)) => {
-                    guilds.set(guilds_response); // Call the success handler
-                    info!("Guilds get successful");
+            match discord_api::get_guilds(discord_token.to_string()).await {
+                Ok((discord_guilds_response)) => {
+                    discord_guilds.set(discord_guilds_response); // Call the success handler
+                    info!("discord_guilds get successful");
                 }
                 Err(e) => {
                     login_error.set(Some(e.to_string()));
-                    info!("Guilds get failed: {}", e);
+                    info!("discord_guilds get failed: {}", e);
                 }
             }
         });
@@ -130,22 +143,33 @@ fn Login(show_login_pane: Signal<bool>, show_server_pane: Signal<bool>, token: S
 
     rsx! {
         div {
-            h1 { style: "color: white;", "Login" }
+            class: format_args!("discord-login {}", if show_discord_login_pane() { "visible" } else { "" }),
+            img {
+                src: "assets/discord_logo.png",
+                alt: "Discord Logo",
+                width: "50px",
+                height: "50px",
+            }
             button {
                 style: "position: absolute; top: 10px; right: 10px; background-color: red; color: white;",
-                onclick: move |_| show_login_pane.set(false),
+                onclick: move |_| {show_login_pane.set(false); show_discord_login_pane.set(false)},
                 "X"
             }
             input {
+                class: "login-input",
                 value: "{username}",
                 oninput: move |event| username.set(event.value())
             }
             input {
+                class: "login-input",
                 r#type: "password",
                 value: "{password}",
                 oninput: move |event| password.set(event.value())
             }
-            button { onclick: handle_login, "Login" }
+            button { 
+                class: "login-button",
+                onclick: handle_login, "Login" 
+            }
 
             if let Some(error) = login_error() {
                 p { "Login failed: {error}" }
@@ -157,8 +181,8 @@ fn Login(show_login_pane: Signal<bool>, show_server_pane: Signal<bool>, token: S
 
 
 #[component]
-fn BottomPane(show_server_pane: Signal<bool>, guilds: Signal<Value>, token: Signal<String>) -> Element {
-    let guilds_array = guilds().as_array().unwrap_or(&vec![]).clone();
+fn DiscordBottomPane(show_discord_server_pane: Signal<bool>, discord_guilds: Signal<Value>, discord_token: Signal<String>) -> Element {
+    let discord_guilds_array = discord_guilds().as_array().unwrap_or(&vec![]).clone();
     let mut channels = use_signal(|| None::<Value>);
     let mut fetch_error = use_signal(|| None::<String>);
     let mut show_channel_pane = use_signal(|| false);
@@ -166,7 +190,7 @@ fn BottomPane(show_server_pane: Signal<bool>, guilds: Signal<Value>, token: Sign
     // Fetch the channels for the selected guild
     let handle_get_channels = move |guild_id: String| {
         block_on(async move {
-            match discord_api::get_guild_channels(token.to_string(), guild_id).await {
+            match discord_api::get_guild_channels(discord_token.to_string(), guild_id).await {
                 Ok(channels_data) => {
                     channels.set(Some(channels_data));
                     show_channel_pane.set(true);
@@ -182,15 +206,16 @@ fn BottomPane(show_server_pane: Signal<bool>, guilds: Signal<Value>, token: Sign
     rsx! {
         div {
             class: {
-                format_args!("bottom-pane {}", if show_server_pane() { "show" } else { "" })
+                format_args!("discord-bottom-pane {}", if show_discord_server_pane() { "show" } else { "" })
             },
-            if !guilds().is_null() {
-                // Render the guilds data
+            h2 { "Discord Servers" }
+            if !discord_guilds().is_null() {
+                // Render the discord_guilds data
                 ul {
                     class: "guild-list",
-                    for guild in guilds_array {
+                    for guild in discord_guilds_array {
                         li {
-                            class: "server-item",
+                            class: "guild-item",
                             button {
                                 class: "guild-button",  // You can style this button as you like in CSS
                                 onclick: move |_| handle_get_channels(guild["id"].as_str().unwrap().to_string()) ,
@@ -200,19 +225,20 @@ fn BottomPane(show_server_pane: Signal<bool>, guilds: Signal<Value>, token: Sign
                     }
                 }
             } else {
-                p { "No guilds available." }
+                p { "No discord_guilds available." }
             }
             ChannelList {
-                token: token.clone(),
+                discord_token: discord_token.clone(),
                 channels: channels.clone(),
-                show_channel_pane: show_channel_pane.clone()
+                show_channel_pane: show_channel_pane.clone(),
+                show_discord_server_pane: show_discord_server_pane.clone()
             }
         }
     }
 }
 
 #[component]
-fn ChannelList(token: Signal<String>, channels: Signal<Option<Value>>, show_channel_pane: Signal<bool>) -> Element {
+fn ChannelList(discord_token: Signal<String>, channels: Signal<Option<Value>>, show_channel_pane: Signal<bool>, show_discord_server_pane: Signal<bool>) -> Element {
     let channels_array = channels()?.as_array().unwrap_or(&vec![]).clone();
     let mut messages = use_signal(|| None::<Value>);
     let mut fetch_error = use_signal(|| None::<String>);
@@ -224,7 +250,7 @@ fn ChannelList(token: Signal<String>, channels: Signal<Option<Value>>, show_chan
         let channel_id_clone = channel_id.clone();
 
         block_on(async move {
-            match discord_api::get_messages(token.to_string(), channel_id).await {
+            match discord_api::get_messages(discord_token.to_string(), channel_id).await {
                 Ok(messages_data) => {
                     messages.set(Some(messages_data));
                     current_channel_id.set(channel_id_clone);
@@ -241,7 +267,7 @@ fn ChannelList(token: Signal<String>, channels: Signal<Option<Value>>, show_chan
     rsx! {
         div {
             class: {
-                format_args!("channel-list-pane {}", if show_channel_pane() { "show" } else { "" })
+                format_args!("channel-list-pane {}", if show_channel_pane() && show_discord_server_pane() { "show" } else { "" })
             },
             h2 { "Channels" }
             button {
@@ -265,10 +291,11 @@ fn ChannelList(token: Signal<String>, channels: Signal<Option<Value>>, show_chan
                 }
             }
             ChannelMessages {
-                token: token.clone(),
+                discord_token: discord_token.clone(),
                 messages: messages.clone(),
                 show_channel_messages_pane: show_channel_messages_pane.clone(),
-                current_channel_id: current_channel_id
+                current_channel_id: current_channel_id,
+                show_discord_server_pane: show_discord_server_pane.clone()
             }
         }
     }
@@ -278,13 +305,13 @@ fn ChannelList(token: Signal<String>, channels: Signal<Option<Value>>, show_chan
 struct EmptyStruct {} // Empty struct to use for coroutines (when you don't need to send anything into the coroutine)
 
 #[component]
-fn ChannelMessages(token: Signal<String>, messages: Signal<Option<Value>>, show_channel_messages_pane: Signal<bool>, current_channel_id: Signal<String>) -> Element {
+fn ChannelMessages(discord_token: Signal<String>, messages: Signal<Option<Value>>, show_channel_messages_pane: Signal<bool>, current_channel_id: Signal<String>,  show_discord_server_pane: Signal<bool>) -> Element {
     let mut send_error = use_signal(|| None::<String>);
     let mut message_input = use_signal(|| "Enter your message here. ".to_string());
 
     let handle_send_message = move |_| {
         block_on(async move {
-            match discord_api::send_message(token.to_string(), current_channel_id.to_string(), message_input.to_string()).await {
+            match discord_api::send_message(discord_token.to_string(), current_channel_id.to_string(), message_input.to_string()).await {
                 Ok((send_response)) => {
                     info!("Message sent successfully");
                 }
@@ -296,7 +323,7 @@ fn ChannelMessages(token: Signal<String>, messages: Signal<Option<Value>>, show_
         });
 
         block_on(async move {
-            match discord_api::get_messages(token.to_string(), current_channel_id.to_string()).await {
+            match discord_api::get_messages(discord_token.to_string(), current_channel_id.to_string()).await {
                 Ok((send_response)) => {
                     messages.set(Some(send_response));
                     info!("Messages update successful");
@@ -317,11 +344,11 @@ fn ChannelMessages(token: Signal<String>, messages: Signal<Option<Value>>, show_
                 tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
 
                 // Fetch updated messages
-                let token_clone = token.to_owned();
+                let discord_token_clone = discord_token.to_owned();
                 let current_channel_id_clone = current_channel_id.to_owned();
                 let mut messages_clone = messages.to_owned();
 
-                match discord_api::get_messages(token_clone.to_string(), current_channel_id_clone.to_string()).await {
+                match discord_api::get_messages(discord_token_clone.to_string(), current_channel_id_clone.to_string()).await {
                     Ok(updated_messages) => {
                         messages_clone.set(Some(updated_messages)); // Update messages with the latest data
                     }
@@ -338,7 +365,7 @@ fn ChannelMessages(token: Signal<String>, messages: Signal<Option<Value>>, show_
     rsx! {
         div {
             class: {
-                format_args!("channel-messages-list-pane {}", if show_channel_messages_pane() { "show" } else { "" })
+                format_args!("channel-messages-list-pane {}", if show_channel_messages_pane() && show_discord_server_pane() { "show" } else { "" })
             },
             h2 { "Messages" }
             button {
@@ -359,11 +386,18 @@ fn ChannelMessages(token: Signal<String>, messages: Signal<Option<Value>>, show_
                         }
                     }
                 }
-                input {
-                    value: "{message_input}",
-                    oninput: move |event| message_input.set(event.value())
+                div {
+                    class: "message-input-container",
+                    input {
+                        class: "message-input-box",
+                        value: "{message_input}",
+                        oninput: move |event| message_input.set(event.value())
+                    }
+                    button {  
+                        class: "send-button", 
+                        onclick: handle_send_message, "Send message" 
+                    }
                 }
-                button { onclick: handle_send_message, "Send message" }
             }
         }
     }
